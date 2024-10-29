e5c81f236c24c3aaf5512655ee472fce8ba94d75
nailaahtarieva <nuscha2006@mail.ru>
2024-10-29 22:52:39 +0500

    Task7

---
+++ Tyuiu.AkhtarievaNR.Sprint2.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.AkhtarievaNR.Sprint2.Task7.V1.Lib
+{
+    public class DataService : ISprint2Task7V1
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((((x > 0) && (x <= 1)) || ((x <= 0) && (x >= -1))) && ((y > 0) && (y <= 1)) && ((x==y)||(-x==y))))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.AkhtarievaNR.Sprint2.Task7.V1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.AkhtarievaNR.Sprint2.Task7.V1.Lib;
+
+namespace Tyuiu.AkhtarievaNR.Sprint2.Task7.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Ахтариева Н. Р. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание: 7                                                              *");
+            Console.WriteLine("* Вариант: 1                                                              *");
+            Console.WriteLine("* Выполнила: Ахтариева Наиля Руслановна | ИИПб-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя значение X и Y и *");
+            Console.WriteLine("* вычисляет, находится ли точка X,Y в заштрихованной области              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится в закрашенной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в закрашенной области");
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
